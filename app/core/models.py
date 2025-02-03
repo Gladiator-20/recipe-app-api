0657a41101350375530d9b079f03dbbbe83d316b
Aashutosh <parohaaashutosh@gmail.com>
2025-02-04 02:15:30 +0530

    Added image upload api

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,9 @@
 """
 Database Models
 """
+import uuid
+import os
+
 from django.db import models
 from app import settings
 
@@ -9,6 +12,13 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -56,7 +66,8 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
-    ingridients = models.ManyToManyField('Ingridients')
+    ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
@@ -74,7 +85,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-class Inridient(models.Model):
+class Ingredient(models.Model):
     """Ingridient for recipes."""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
